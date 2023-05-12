4d435a5900ad31b82fedc342ff31f25cb04d58ff
Ethan Black <ethanrossblack@gmail.com>
2023-05-12 15:36:37 -0600

    Add initial test for linked_list

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -10,39 +10,5 @@ describe LinkedList do
     expect(list).to be_a(LinkedList)
   end
 
-  it "starts with an empty head" do
-    list = LinkedList.new
-    
-    expect(list.head).to be nil
-  end
-  
-  it "can append one new node" do
-    list = LinkedList.new
-    list.append("doop")
-    
-    expect(list.head).to be_a(Node)
-  end
-  
-  it "can point to a nil tail with a single head node" do
-    list = LinkedList.new
-    list.append("doop")
-    
-    expect(list.head.next_node).to be nil
-  end
-  
-  it "can count how many things are in the list" do
-    list = LinkedList.new
-    list.append("doop")
-    # require "pry"; binding.pry
-    expect(list.count).to eq(1)
-  end
-  
-  it "can return a string of the first node" do
-    list = LinkedList.new
-    list.append("doop")
-
-    expect(list.to_string).to eq("doop")
-  end
-
 end
 
