d5b16020bdac2b54c63ae2d78701ebc93550bf0b
Ethan Black <ethanrossblack@gmail.com>
2023-05-12 13:40:01 -0600

    Add next_node attribute to Node class and accompanying tests

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,11 +2,24 @@ require "./lib/node"
 require "rspec"
 
 describe Node do
+
   it "exists" do
-    node = Node.new("bloop")
+    node = Node.new("plop")
 
     expect(node).to be_a(Node)
   end
 
+  it "can return data" do
+    node = Node.new ("plop")
+    
+    expect(node.data).to eq("plop")
+  end
   
+  it "points to the next node" do
+    node = Node.new ("plop")
+    
+    expect(node.next_node).to be nil
+  end
+
+
 end
