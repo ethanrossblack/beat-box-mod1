6ead6c45aecb5006182e4521950c41c38797715c
Ethan Black <ethanrossblack@gmail.com>
2023-05-12 15:39:29 -0600

    Add initialize method to LinkedList class, remove previous methods for cleaner commit history

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,20 +1,5 @@
 class LinkedList
-  attr_reader :head
-
   def initialize
     @head = nil
-  end
-
-  def append(data)
-    node = Node.new(data)
-    @head = node
-  end
-
-  def count
-    1
-  end
-
-  def to_string
-    @head.data.to_s
   end
 end
