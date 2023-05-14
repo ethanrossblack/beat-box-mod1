5783b871c29358c25bb3bb63147f2a5c7b39b150
Ethan Black <ethanrossblack@gmail.com>
2023-05-13 18:38:52 -0600

    Add a test that checks that a new BeatBox instance also creates an empty LinkedList instance

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -9,4 +9,11 @@ describe BeatBox do
 
     expect(bb).to be_a(BeatBox)
   end
+
+  it "creates an empty LinkedList item on initialization" do
+    bb = BeatBox.new
+
+    expect(bb.list).to be_a(LinkedList)
+    expect(bb.list.head).to be nil
+  end
 end
