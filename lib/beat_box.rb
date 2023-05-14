7781b642f36accadcb1d350398db242863087b0a
Ethan Black <ethanrossblack@gmail.com>
2023-05-13 18:50:22 -0600

    Add append method to BeatBox class

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,4 +4,14 @@ class BeatBox
   def initialize
     @list = LinkedList.new
   end
+
+  def append(data)
+    return if !data.is_a?(String)
+
+    beats = data.split(" ")
+    beats.each do |beat|
+      @list.append(beat)
+    end
+    data
+  end
 end
