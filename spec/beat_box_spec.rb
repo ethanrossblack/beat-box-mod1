5ac5c736619797d8131b45e362e89ea4946c01ca
Ethan Black <ethanrossblack@gmail.com>
2023-05-16 16:14:15 -0600

    test: changed test variables to instance variables.

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -7,44 +7,37 @@ describe BeatBox do
   before(:each) do
     @bb = BeatBox.new
   end
-  
-  it "exists" do
-    bb = BeatBox.new
 
-    expect(bb).to be_a(BeatBox)
+  it "exists" do
+    @expect(bb).to be_a(BeatBox)
   end
 
   it "creates an empty LinkedList item on initialization" do
-    bb = BeatBox.new
-
-    expect(bb.list).to be_a(LinkedList)
-    expect(bb.list.head).to be nil
+    expect(@bb.list).to be_a(LinkedList)
+    expect(@bb.list.head).to be nil
   end
 
   it "can append multiple nodes at a time" do
-    bb = BeatBox.new
-    bb.append("deep doo ditt")
+    @bb.append("deep doo ditt")
     
-    expect(bb.list.head.data).to eq("deep")
-    expect(bb.list.head.next_node.data).to eq("doo")
+    expect(@bb.list.head.data).to eq("deep")
+    expect(@bb.list.head.next_node.data).to eq("doo")
   end
 
   it "can count the number of nodes" do
-    bb = BeatBox.new
-    bb.append("deep doo ditt")
+    @bb.append("deep doo ditt")
 
-    expect(bb.count).to eq(3)
+    expect(@bb.count).to eq(3)
 
-    bb.append("woo hoo shu")
+    @bb.append("woo hoo shu")
 
-    expect(bb.count).to eq(6)
+    expect(@bb.count).to eq(6)
   end
 
   it "can play beats" do
-    bb = BeatBox.new
-    bb.append("deep doo ditt")
+    @bb.append("deep doo ditt")
 
-    expect(bb.play).to eq(3)
+    expect(@bb.play).to eq(3)
   end
 
   it "can add beats on initialization" do
